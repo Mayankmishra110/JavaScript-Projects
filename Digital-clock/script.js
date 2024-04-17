f7f505dcb7e963a33529de40b74692380ec99fd1
Mayankmishra110 <kmmay15@gmail.com>
2024-04-17 14:48:34 +0530

    digital clock done

--- Digital-clock/script.js
+++ Digital-clock/script.js
@@ -1,24 +1,6 @@
-const buttons = document.querySelectorAll(".button");
-const body = document.querySelector("body");
+const clock = document.querySelector(".clock");
 
-buttons.forEach(function (button) {
-  console.log(button);
-
-  button.addEventListener("click", function (e) {
-    console.log(e);
-    console.log(e.target);
-
-    if (e.target.id === "grey") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "pink") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "blue") {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === "yellow") {
-      body.style.backgroundColor = e.target.id;
-    }
-  });
-});
+setInterval(function () {
+  let date = new Date();
+  clock.innerHTML = date.toLocaleTimeString();
+}, 1000);
